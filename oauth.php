dfe1097204570f5db84302efb68b11b2f24c7816
ntundt <nikita.tihonovich@gmail.com>
2019-02-20 18:14:00 +0300

    oauth.php now uses new architecture

--- oauth.php
+++ oauth.php
@@ -1,43 +1,12 @@
 <?php
 
-header('Content-Type: text/json; charset=utf-8');
-
 require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/method/api_init.php';
 
-$already_sent = false;
-
-if (0 < count($_POST)) {
-	$parameters = $_POST;
-} else {
-	$parameters = $_GET;
-}
-
-function makeResponse($response = [], $error = []) {
-	global $already_sent;
-	if (!$already_sent) {
-		$return_object = [];
-		if (0 === strcmp(gettype($response), 'array')) {
-			if (0 < count(array_keys($response))) {
-				$return_object['response'] = $response;
-			}
-		} else {
-			$return_object['response'] = $response;
-		}
-		if (0 === strcmp(gettype($error), 'array')) {
-			if (0 < count(array_keys($error))) {
-				$return_object['error'] = $error;
-			}
-		} else {
-			$return_object['error'] = $error;
-		}
-		echo json_encode($return_object, JSON_UNESCAPED_UNICODE);
-		$already_sent = true;
-	}
-}
-
-if (isset($parameters['user_id']) and isset($parameters['access_token']) and isset($parameters['email'])) {
-	makeResponse(['sid' => Auth::authByVkId($parameters['user_id'], $parameters['access_token'])], ErrorList::makeAssoc());
-} else {
-	ErrorList::addError(106);
-	makeResponse([], ErrorList::makeAssoc());
-}
+$responseConstructor = new ServerResponse($_POST, $_GET, $_COOKIE);
+$responseConstructor->setRequiredParameters(['user_id', 'access_token', 'email']);
+$responseConstructor->checkParameters(); 
+$responseConstructor->makeResponse(['sid' => Auth::authByVkId(
+	$responseConstructor->requestParameters['user_id'],
+	$responseConstructor->requestParameters['access_token']
+)]);
