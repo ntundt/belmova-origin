4964d37b3001ba52e87ee9dbd43e5b1054321dca
ntundt <nikita.tihonovich@gmail.com>
2019-02-28 15:07:27 +0200

    Fixed issue with require_once on some php configurations

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'config.php';
+require_once __DIR__ . '/config.php';
 
 if (isset($_COOKIE['sid'])) {
 	$user = new User(Auth::getUserId($_COOKIE['sid']));
@@ -12,40 +12,40 @@ if (strpos($_SERVER['REQUEST_URI'], 'bugtracker') !== false) {
 	if (isset($_GET['act'])) {
 		switch ($_GET['act']) {
 		case 'add':
-			include 'markup/bugtracker_add.phtml';
+			include __DIR__ . '/markup/bugtracker_add.phtml';
 			break;
 		case 'view':
-			include 'markup/bugtracker_view.phtml';
+			include __DIR__ . '/markup/bugtracker_view.phtml';
 			break;
 		default:
-			include 'markup/bugtracker.phtml';
+			include __DIR__ . '/markup/bugtracker.phtml';
 			break;
 		}
 	} else {
-		include 'markup/bugtracker.phtml';
+		include __DIR__ . '/markup/bugtracker.phtml';
 	}
 } else if (strpos($_SERVER['REQUEST_URI'], 'login') !== false) {
-	include 'markup/login.phtml';
+	include __DIR__ . '/markup/login.phtml';
 } else if (strpos($_SERVER['REQUEST_URI'], 'index') !== false) {
-	include 'markup/index.phtml';
+	include __DIR__ . '/markup/index.phtml';
 } else if (strpos($_SERVER['REQUEST_URI'], 'learn') !== false) {
 	if (isset($_GET['act'])) {
 		switch ($_GET['act']) {
 		case 'lesson':
-			include 'markup/learn_lesson.phtml';
+			include __DIR__ . '/markup/learn_lesson.phtml';
 			break;
 		case 'constructor':
-			include 'markup/learn_constructor.phtml';
+			include __DIR__ . '/markup/learn_constructor.phtml';
 			break;
 		default:
-			include 'markup/learn.phtml';
+			include __DIR__ . '/markup/learn.phtml';
 			break;
 		}
 	} else {
-		include 'markup/learn.phtml';
+		include __DIR__ . '/markup/learn.phtml';
 	}
 } else if (strpos($_SERVER['REQUEST_URI'], 'oauth') !== false) {
-	include 'oauth.php';
+	include __DIR__ . '/oauth.php';
 } else {
-	include 'markup/index.phtml';
+	include __DIR__ . '/markup/index.phtml';
 }
