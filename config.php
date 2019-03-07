d81740e66361c0c0170bb9f1d8955e4fa604aa5b
ntundt <nikita.tihonovich@gmail.com>
2019-03-07 23:26:06 +0300

    Add consts file import

--- config.php
+++ config.php
@@ -2,15 +2,15 @@
 
 // Database options
 define('DB_HOST', 'localhost');
-define('DB_LOGIN', 'nikita');
+define('DB_LOGIN', 'root');
 define('DB_PASSWORD', 'forestrys');
-define('DB_NAME', 'belmova');
+define('DB_NAME', 'bm');
 define('DB_TABLE_PREFIX', 'bm_');
 
 define('VK_ACCESS_TOKEN', '4ccc6c984ccc6c984ccc6c984a4ca401ec44ccc4ccc6c9810991b8c237d255dcdf1a0b1');
 define('VK_API_VERSION', '5.92');
 
-define('URL_TO_DIR', 'http://bmtemp.herokuapp.com/');
+define('URL_TO_DIR', 'http://localhost/work/');
 
 define('SERVICE_NAME', 'Sample service name');
 
