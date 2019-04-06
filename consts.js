4fd5110d39b2f2bf4409ddb91ee046c2bdc0742c
Mikita Tsikhanovich <nikita.tihonovich@gmail.com>
2019-04-06 22:28:06 +0300

    Remove useless object

--- consts.js
+++ consts.js
@@ -1,8 +1,3 @@
-const LANGRU = {
-	bugtracker: "Баг-трекер",
-	all_reports: "Все отчёты"
-};
-
 const URL = "http://192.168.100.16/";
 const ROOT_URL = URL + "work/";
 const API_URL = URL + "work/method/";
